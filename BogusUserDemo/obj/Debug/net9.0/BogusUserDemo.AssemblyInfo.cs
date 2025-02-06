12959833815e400f5b45ba7634a5aa8491e9a38d
Kardel Rüveyda <33912144+KardelRuveyda@users.noreply.github.com>
2025-02-07 00:46:07 +0300

    Bin obj gitignore

--- BogusUserDemo/obj/Debug/net9.0/BogusUserDemo.AssemblyInfo.cs
+++ BogusUserDemo/obj/Debug/net9.0/BogusUserDemo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BogusUserDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9b4565834fc4ffdf34a2df2ed73dea6acbf910f")]
 [assembly: System.Reflection.AssemblyProductAttribute("BogusUserDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BogusUserDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
