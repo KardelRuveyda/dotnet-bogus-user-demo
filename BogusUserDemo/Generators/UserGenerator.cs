3cc7c4c774cbd9208903b256c1f34f1407f37c4a
Kardel Rüveyda <33912144+KardelRuveyda@users.noreply.github.com>
2025-02-07 00:58:38 +0300

    Bin obj gitignore

---
+++ BogusUserDemo/Generators/UserGenerator.cs
@@ -0,0 +1,16 @@
+﻿using Bogus;
+using BogusUserDemo;
+
+public class UserGenerator : Faker<User>
+{
+    public UserGenerator()
+    {
+        RuleFor(u => u.Id, f => f.Random.Guid());
+        RuleFor(u => u.FirstName, f => f.Name.FirstName());
+        RuleFor(u => u.LastName, f => f.Name.LastName());
+        RuleFor(u => u.Email, (f, u) => f.Internet.Email(u.FirstName, u.LastName));
+        RuleFor(u => u.PhoneNumber, f => f.Phone.PhoneNumber());
+        RuleFor(u => u.BirthDate, f => f.Date.PastDateOnly(30));
+        RuleFor(u => u.Address, f => f.Address.FullAddress());
+    }
+}
---
+++ BogusUserDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        #region Statik Metod Kullanarak Faker Nesnesi Tanımlama
+        // Statik metod kullanarak sahte kullanıcıları oluşturuyoruz
+        var generatorStatic = BogusUserGenerator.CreateFaker(); // Faker<User> nesnesi
+        var usersStatic = generatorStatic.Generate(10); // 10 adet sahte kullanıcı oluşturuyoruz
+
+        // Statik metod ile oluşturduğumuz kullanıcıları yazdırıyoruz
+        Console.WriteLine("Statik Metod ile Oluşturulan Kullanıcılar:");
+        foreach (var user in usersStatic)
+        {
+            Console.WriteLine($"{user.FirstName} {user.LastName} - {user.Email}");
+        }
+        #endregion
+
+        #region Faker Sınıfından Türetilmiş Bir Alt Sınıf Kullanma
+        // UserGenerator sınıfından nesne oluşturuyoruz
+        var generatorSubclass = new UserGenerator(); // UserGenerator nesnesi
+        var usersSubclass = generatorSubclass.Generate(10); // 10 adet sahte kullanıcı oluşturuyoruz
+
+        // Alt sınıf ile oluşturduğumuz kullanıcıları yazdırıyoruz
+        Console.WriteLine("\nFaker Alt Sınıf ile Oluşturulan Kullanıcılar:");
+        foreach (var user in usersSubclass)
+        {
+            Console.WriteLine($"{user.FirstName} {user.LastName} - {user.Email}");
+        }
+        #endregion
+    }
+}
